aabca52afd695bd0528d2fa709bf115ce5e1c693
mayrapick <mayrapiccirilli@gmail.com>
2023-07-12 17:21:58 -0300

    ejecucion para comprimir imagenes

--- comprimir.sh
+++ comprimir.sh
@@ -2,7 +2,7 @@
 
 #VAMOS A GENERAR UN ARCHIVO CON LISTA DE NOMBRES DE TODAS LAS IMAGENES
 
-ls -1 imagenes_dir | awk -F/ '{print $NF}' > lista_nombres_imagenes.txt
+ls -1 imagenes* | awk -F/ '{print $NF}' > lista_nombres_imagenes.txt
 
 # CON LISTA DE NOMBRES VALIDOS:
 
@@ -13,5 +13,7 @@ grep -E '^[A-Z][a-z]+$' lista_nombres_imagenes.txt > lista_nombres_validos.txt
 grep -c 'a$' lista_nombres_validos.txt > total_nombres_en_a.txt
 
 #COMPRIMIR ARCHIVOS Y DIRECTORIOS:
+
 zip comprimidos lista_nombres_imagenes.txt lista_nombres_validos.txt total_nombres_en_a.txt
 
+exit 0 |  echo "se descomprimieron exitosamente los archivos"
